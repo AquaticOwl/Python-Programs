1c5ee8805387242a0ffea5d7128c38ea216228e7
AquaticOwl <lsaddler00@gmail.com>
2017-02-07 08:12:42 -0500

    Delete are_you_awesome.py

--- are_you_awesome.py
+++
@@ -1,8 +0,0 @@
-def areYouAwesome():
-    name = input("name: ")
-    if name == "Shay":
-        print("You are awesome!")
-    else:
-        print("You might be awesome.")
-awesome_test = areYouAwesome
-awesome_test()
